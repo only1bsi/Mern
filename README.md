4ce25a73b20f5bffc552f5cec9f45279446165a3
only1bsi <bukarsamuelisaac@gmail.com>
2023-06-02 10:45:31 -0700

    Initialize project using Create React App

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 # Mern
 =======
+=======
+>>>>>>> 632c6c2 (Initialize project using Create React App)
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -71,4 +74,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+<<<<<<< HEAD
+>>>>>>> 632c6c2 (Initialize project using Create React App)
+=======
 >>>>>>> 632c6c2 (Initialize project using Create React App)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+<<<<<<< HEAD
 import App from './App';
 import axios from 'axios';
 
  
 axios.defaults.baseURL = "http://localhost:5000"
+=======
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+>>>>>>> 632c6c2 (Initialize project using Create React App)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,4 +22,8 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+<<<<<<< HEAD
 
+=======
+reportWebVitals();
+>>>>>>> 632c6c2 (Initialize project using Create React App)
